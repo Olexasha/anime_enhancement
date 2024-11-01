6b6c11d499a154228ef412a9a5bcd5d91214afa2
Olexasha <MrDarwin1859@yandex.ru>
2024-11-01 19:42:41 +0300

    Иницилизация проекта

---
+++ config.py
@@ -0,0 +1,16 @@
+# Параметры и пути
+ORIGINAL_VIDEO = "/home/uzver_pro/Videos/war2.mp4"
+ORIGINAL_AUDIO = "/home/uzver_pro/Videos/soundtrack.aac"
+TMP_FINAL_VIDEO = "/media/uzver_pro/7d3a855c-208a-4e34-9b13-1f70d2e178e5/anime_upscaling/tmp_final_video.mp4"
+FINAL_VIDEO = "/media/uzver_pro/7d3a855c-208a-4e34-9b13-1f70d2e178e5/anime_upscaling/final_video.mp4"
+BATCH_VIDEO_PATH = "/media/uzver_pro/7d3a855c-208a-4e34-9b13-1f70d2e178e5/anime_upscaling"
+INPUT_BATCHES_DIR = "/home/uzver_pro/Video_Montage/batches"
+OUTPUT_BATCHES_DIR = "/media/uzver_pro/7d3a855c-208a-4e34-9b13-1f70d2e178e5/anime_upscaling/batches"
+
+START_BATCH_TO_UPSCALE = 7
+END_BATCH_TO_UPSCALE = 0
+STEP_PER_BATCH = 6
+FRAMES_PER_BATCH = 1000
+
+FPS = 23.98
+MERGE_ALL_BATCHES_TO_VIDEO = True
